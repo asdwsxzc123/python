ec809e1468e60511b3f295d2e8c77447a9612f07
asdwsxzc123 <sbfgeyygy@126.com>
2018-10-30 13:28:25 +0800

    继承

--- 10/01隐藏对象的属性.py
+++ 10/01隐藏对象的属性.py
@@ -1,4 +1,11 @@
-class Dog:
+""" 
+私有属性
+私有方法
+调用私有方法
+del方法
+引用计数
+ """
+""" class Dog:
     def set_age(self, new_age):
         if new_age > 0 and new_age <= 100:
             self.age = new_age
@@ -13,3 +20,47 @@ dog = Dog()
 dog.set_age(15)
 age = dog.get_age()
 print(age)
+
+ """
+
+""" 
+class Dog:
+    def __init__(self):
+        self.num1 = 100
+        # 私有属性
+        self.__num2 = 200
+    #  私有方法 __
+    def __send_msg(self):
+        print('--正在发短信---')
+    # 共有方法
+
+    def send_msg(self, new_money):
+        if new_money > 10000:
+            self.__send_msg()
+        else:
+            print('---共有方法---')
+
+
+dog = Dog()
+dog.send_msg(100000) """
+
+""" del 方法 """
+
+""" 
+class Dog:
+	# 删除对象时会调用
+	def __del__(self):
+		print('---英雄over---')
+
+dog1 = Dog()
+# 指针指向dog1
+dog2 = dog1
+# 对象的引用计数
+import sys
+
+count = sys.getrefcount(dog2)
+print(count)
+del dog1
+del dog2
+print('----------') """
+
---
+++ 10/02继承.py
@@ -0,0 +1,67 @@
+""" 
+继承
+重写
+调用父类
+私有方法在继承中的表现
+多继承
+ """
+""" # 基类
+class Animal:
+    def __init__(self):
+        self.num1 = 100
+        self.__num2 = 200
+    def eat(self):
+        print('吃')
+    def __test(self):
+        print('我是私有方法')
+
+# 继承
+# 子类
+class Dog(Animal):
+    def bark(self):
+        print('--汪汪--')
+
+class Xiaotq(Dog):
+    def fly(self):
+        print('---飞---')
+    # 重写
+    def bark(self):
+        print('---我汪---')
+        # 第一种.调用父类的方法 
+        # Dog.bark(self)
+
+        # 第二种.调用父类的方法 
+        super().bark()
+
+xiaotianq = Xiaotq()
+xiaotianq.bark()
+xiaotianq.eat() """
+
+# 多继承
+# 顶层继承object，新式类， 什么都不写经典类（过时）
+
+
+class Base(object):
+    def test(self):
+        print('--base')
+
+
+class A(Base):
+    def test1(self):
+        print('---test1')
+
+
+class B(Base):
+    def test2(self):
+        print('--test2')
+
+
+class C(A, B):
+    pass
+
+
+a = A()
+a.test()
+c = C()
+c.test1()
+c.test2()
