be21cfe07ca0c48e09fd1ef1f66523624fb99b8b
asdwsxzc123 <sbfgeyygy@126.com>
2018-11-15 13:29:19 +0800

    优化

--- advanced/05/02select.sql
+++ advanced/05/02select.sql
@@ -22,8 +22,8 @@ create table classes(
 );
 
 insert into students values 
-(0, '小明', 18, 180.00,2,1,0),
-(0, '小月月', 18, 180.00,2,2,0),
+(0, '小明', 18, 180.00,2,4,0),
+(0, '小月月', 18, 180.00,2,5,0),
 (0, '彭于晏', 29, 185.00,1,2,0),
 (0, '刘德华', 59, 175.00,1,1,0),
 (0, '黄蓉', 38, 150.00,2,1,0),
@@ -75,6 +75,8 @@ select * from students where age = 18
 -- 年龄不是小于或者等于18,并且是女性
 select * from student where (not age <= 18)  and gender= 2;
 
+
+
 -- 模糊查询
 --  like 
 -- %替换1个或多个
@@ -94,6 +96,7 @@ select name from students where name rlike '^周.*';
 select name from students where name rlike '^周.*伦$';
 
 
+
 -- 范围查询
 --  in(1,3,8)表示一个非连续的范围内
 -- 查询两年在18,34的
@@ -109,10 +112,12 @@ select name ,age from students where age not BETWEEN 18 and 34;
 select name ,age from students where not age BETWEEN 18 and 34;
 
 
+
 -- 空判断
 -- is null 
 select * from students where height is null;
 -- is not null
+
 
 
 /* 排序 */
@@ -122,6 +127,8 @@ select * from students where height is null;
 select * from students where (age between 18 and 30) and gender = 1 order by age;
 
 select * from students where (age between 18 and 30) and gender = 1 order by age desc, id desc;
+
+
 
 /* 聚合函数 */
 -- count
@@ -143,6 +150,7 @@ select sum(age)/count(*) from students;
 select round(sum(age)/count(*), 2) as 平均值 from students;
 
 
+
 /* 分组 */
 --  group by
 -- 按照性别分组,查询所有的性别
@@ -152,9 +160,10 @@ select gender,count(*) from students group by gender;
 select gender,group_concat(name, "_", age) from students where gender=1 group by gender;
 
 -- having 可以过滤
-select gender,group_concat(name) from students group by gender having avg(age)>30;
+select gender,group_concat(name,age) from students group by gender having avg(age)>30;
 
 select gender ,group_concat(name) from students group by gender HAVING count(*) > 2;
+
 
 
 /* 分页 */
@@ -166,6 +175,7 @@ select * from students limit 5, 2;
 -- select * from student limit n-1, count
 
 
+
 /* 连接查询 */
 -- inner join .. on
 select * from students inner join classes where students.cls_id = classes.id
@@ -175,3 +185,33 @@ select students.*,classes.name from students inner join classes on students.cls_
 
 -- 取别名
 select s.*,c.name from students as s inner join classes as c on s.cls_id=c.id;
+
+-- 以班级排序并有课程的
+-- 内连接
+select c.name, s.* from students as s inner join classes as c on s.cls_id = c.id order by c.name asc;
+
+-- 左连接,以students的表为基准,找不到的为null
+select * from students as s left join classes as c on s.cls_id = c.id ;
+
+-- 右连接,以classes为基准,找不到为null(基本没用)
+select * from students as s right join classes as c on s.cls_id = c.id;
+
+
+
+/* 自关联 */
+
+create table areas(
+  aid int primary key,
+  atitle varchar(20),
+  pid int
+)
+-- 先查山东pid,然后根据pid查
+select aid from areas where atitle='山东省';
+select * from areas where pid=370200;
+
+-- 一次查出来
+select province.name from areas as province inner join areas as city  on city.pid =province.aid having province.atitle = '山东省'
+
+/* 子查询 */
+--  标量子查询,先执行子查询,在执行复查下
+select * from students where height = (select max(height) from students);
