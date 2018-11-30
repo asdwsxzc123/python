46b28cac89edbfe13c394cf50a2e8b37b567462b
asdwsxzc123 <sbfgeyygy@126.com>
2018-11-30 13:32:38 +0800

    flask

---
+++ advanced/09flask/01.py
@@ -0,0 +1,18 @@
+""" flask """
+# 轻量级框架,需要使用拓展包
+# flask-sqlalchemy 数据库操作
+# flask-migrate 管理迁移数据库
+# flask-mail 邮件
+# flask-wtf 表单
+# flask-script 插入脚本
+# flask-login 认证用户状态
+# flask-restful: 开发rest api
+# flask-bootstrap
+# flask-momnet 本地化日期和时间
+
+
+# 将包导出
+# pip freeze > requirements.txt
+
+# 安装包文件
+# pip install -r requirements.txt
